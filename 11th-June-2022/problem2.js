27b7883120606cd812fe84fd53c6c116edebf8d3
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-11 20:03:57 +0530

    solved problem 2

---
+++ 11th-June-2022/problem2.js
@@ -0,0 +1,39 @@
+/* 
+
+PROBLEM DESCRIPTION:
+
+Suzuki is a monk who climbs a large staircase to the monastery as part of a ritual. Some days he climbs more stairs than others depending on the number of students he must train in the morning. He is curious how many stairs might be climbed over the next 20 years and has spent a year marking down his daily progress.
+
+The sum of all the stairs logged in a year will be used for estimating the number he might climb in 20.
+
+20_year_estimate = one_year_total * 20
+
+You will receive the following data structure representing the stairs Suzuki logged in a year. You will have all data for the entire year so regardless of how it is logged the problem should be simple to solve.
+
+stairs = [sunday,monday,tuesday,wednesday,thursday,friday,saturday]
+Make sure your solution takes into account all of the nesting within the stairs array.
+
+Each weekday in the stairs array is an array.
+
+sunday = [6737, 7244, 5776, 9826, 7057, 9247, 5842, 5484, 6543, 5153, 6832, 8274, 7148, 6152, 5940, 8040, 9174, 7555, 7682, 5252, 8793, 8837, 7320, 8478, 6063, 5751, 9716, 5085, 7315, 7859, 6628, 5425, 6331, 7097, 6249, 8381, 5936, 8496, 6934, 8347, 7036, 6421, 6510, 5821, 8602, 5312, 7836, 8032, 9871, 5990, 6309, 7825]
+Your function should return the 20 year estimate of the stairs climbed using the formula above.
+
+*/
+
+// Have to calculate the total stairs he climbed in a year.
+// The data structure for the notes are in the format:
+// stairs = [sunday,monday,tuesday,wednesday,thursday,friday,saturday]
+// The data structure for every stair count is:
+// sunday = [6737, 7244, 5776, 9826, 7057, 9247, 5842, 5484, 6543, 5153, 6832, 8274, 7148, 6152, 5940, 8040, 9174, 7555, 7682, 5252, 8793, 8837, 7320, 8478, 6063, 5751, 9716, 5085, 7315, 7859, 6628, 5425, 6331, 7097, 6249, 8381, 5936, 8496, 6934, 8347, 7036, 6421, 6510, 5821, 8602, 5312, 7836, 8032, 9871, 5990, 6309, 7825]
+
+// FINAL STEP: Need to multiply 20 * steps in a year
+// STEP BEFORE THAT: Need to first calculate steps in a year
+// STEP BEFORE THAT: Need to add total number of days in a week [with their corresponding yearly values]
+// Need to calculate total number of steps in a day
+// Return the values.
+
+const stairsIn20 = (s) => 20 * s.reduce((acc, day) => {
+    return acc + day.reduce((acc2, stepsOnDay) => {
+      return acc2 + stepsOnDay
+    }, 0)
+  }, 0)

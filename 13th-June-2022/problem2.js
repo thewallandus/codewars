aaaecf821a28ac13286099120acda3495f4642d4
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-13 20:49:47 +0530

    problem 2

---
+++ 13th-June-2022/problem2.js
@@ -0,0 +1,6 @@
+//Arrays
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+
+function reverseList(list) {
+    console.log(list.reverse())
+}

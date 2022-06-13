8d9e26a915e09b6b3f18051a8998fbd4e276e285
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-13 20:51:16 +0530

    problem 3

---
+++ 13th-June-2022/problem3.js
@@ -0,0 +1,10 @@
+
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+
+function sum(array) {
+
+    return newArray.reduce((acc, element) => {
+        return acc + Number(element)
+    }, 0)
+
+}

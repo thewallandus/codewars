4338638c95d95201d2abb56dfb3574dd20eb029b
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-19 18:04:22 +0530

    problem 2 solution

--- 19th-June-2022/problem2.js
+++ 19th-June-2022/problem2.js
@@ -10,7 +10,10 @@ Return True if yes, False otherwise :)
 // OR ELSE NO
 
 function hero(bullets, dragons){
- 
 
-
+if (bullets >= (dragons * 2)) {
+    return true
+} else {
+    return false
+}
 }

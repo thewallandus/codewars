f1689498bb6a3b4be392de4977c69157564740ce
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-20 16:42:06 +0530

    problem 1 solution

--- 20th-June-2022/problem1.js
+++ 20th-June-2022/problem1.js
@@ -10,6 +10,7 @@ m = 1
 s = 1
 
 result = 61000
+
 Input constraints:
 
 0 <= h <= 23
@@ -23,6 +24,13 @@ Input constraints:
 // Clock shows s seconds.
 // Create a function.
 // This function will take in 3 parameters: hours, minutes, seconds.
-// Then it will multiply and convert the parameters into seconds.
-// Convert this number/variable into 1000 miliseconds.
-// Return.
+// Create 3 variables that convert the total number of hours, minutes, and seconds into milliseconds.
+// Then return the sum of these 3 variables.
+
+function past(hours, minutes, seconds) {
+    const totalHoursInMs = hours * 60 * 60 * 1000;
+	const totalMinutesInMs = minutes * 60 * 1000;
+    const secondsInMs = seconds * 1000;
+  
+  return totalHoursInMs + totalMinutesInMs + secondsInMs
+}

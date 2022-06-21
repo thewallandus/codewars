3b244272742bc3486d21c3c335efaf123c053df1
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-21 15:52:56 +0530

    problem 1 solution

--- 21st-June-2022/problem1.js
+++ 21st-June-2022/problem1.js
@@ -12,3 +12,17 @@ Note: Empty arrays should return 0.
 // Run a loop through the array: and sum all the nums and store it in a variable // reduce method?
 // Edge cases: empty arrays should return 0 ie array.length is 0.
 //Make sure every element is a number: Use Number() method.
+
+
+
+function find_average(array) {
+    if (array.length === 0 ) {
+      return 0
+    } else {
+         let sum = array.reduce((acc, element) => {
+        return acc + element;
+      }, 0)
+      let average = sum / array.length
+      return average
+      } 
+  }

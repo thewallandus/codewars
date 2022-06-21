43ae55d38309376a00b968f8e998528f66aef1fb
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-21 16:19:54 +0530

    problem 2 solution

--- 21st-June-2022/problem2.js
+++ 21st-June-2022/problem2.js
@@ -8,10 +8,15 @@ Example : n=5 --> [5,4,3,2,1]
 
 // PSEUDOCODE: Build a function.
 // Builds out an array of integers.
-// Create a loop: for or map?
+// Create an empty array.
+// Create a loop: for that reverses.
 // That has an array of integers from n to 1 where n > 0.
 
 
 const reverseSeq = n => {
-    
+    let arr = [];
+    for (let i = n; i >= 1; i--) {
+       arr.push(i)
+    }
+  	return arr
   };

5eeb72490127b29bc10af5801a75ad597fa05f0a
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-21 16:05:25 +0530

    pseudocode problem 2 verson 1

--- 21st-June-2022/problem2.js
+++ 21st-June-2022/problem2.js
@@ -5,3 +5,13 @@ Build a function that returns an array of integers from n to 1 where n>0.
 Example : n=5 --> [5,4,3,2,1]
 
 */
+
+// PSEUDOCODE: Build a function.
+// Builds out an array of integers.
+// Create a loop: for or map?
+// That has an array of integers from n to 1 where n > 0.
+
+
+const reverseSeq = n => {
+    
+  };

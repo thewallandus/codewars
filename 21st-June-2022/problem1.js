9572a945141a832e7f517d7e6870607a43bdbb18
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-21 15:51:14 +0530

    problem 1 pseudo code

--- 21st-June-2022/problem1.js
+++ 21st-June-2022/problem1.js
@@ -5,3 +5,10 @@ Write a function which calculates the average of the numbers in a given list.
 Note: Empty arrays should return 0.
 
 */
+
+// PSEUDOCODE
+// Take in a list of numbers: clearly an array.
+// Average formula = sum the total numbers / total number of numbers [or array.length]
+// Run a loop through the array: and sum all the nums and store it in a variable // reduce method?
+// Edge cases: empty arrays should return 0 ie array.length is 0.
+//Make sure every element is a number: Use Number() method.

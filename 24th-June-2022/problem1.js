afa25c154eec7976da399be5d258051e46eb1b19
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-23 15:31:47 +0530

    problem 1 solution

--- 24th-June-2022/problem1.js
+++ 24th-June-2022/problem1.js
@@ -11,9 +11,16 @@ Return true if the array contains the value, false if not.
 // Given an array a
 // Given a value x which can be numbers or strings
 // Check the array with a loop
-// If it has the value: return true
-// Else return false
+// If it has the value: return true [conditional]
+// Else return false [by default]
 
-function check(a, x) {
-    
-  }
+function check(a,x){
+    let result = false;
+    for(i = 0; i < a.length; i++){
+      if(a[i]==x){
+      result  = true;
+      }
+     
+    }
+    return result;
+  };

8c455068ff65546a903b4dea7691e5cdd57be198
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-25 10:35:28 +0530

    problem 1 solution first try

--- 26th-June-2022/problem1.js
+++ 26th-June-2022/problem1.js
@@ -6,6 +6,18 @@ Return the average of the given array rounded down to its nearest integer.
 
 The array will never be empty.
 
-
-
 */
+
+// P: parameters: an array that will never be empty
+// R: returns: return the average rounded to its nearest integer
+// E: examples: [2,2,2,2]), 2, [1,2,3,4,5,]),3
+// P: pseudocode
+
+function getAverage(marks){
+    // sum out all of the elements of the array
+    let sum = marks.reduce((acc, elements) => {
+        return acc + elements
+    }, 0)
+    // Divide it by the total length
+    return Math.floor(sum / marks.length)
+}

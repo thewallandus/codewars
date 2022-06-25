0c2a54b66fc8c878c7505d0ccad7047eb9191793
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-25 10:51:54 +0530

    problem 2 solution first try

--- 26th-June-2022/problem2.js
+++ 26th-June-2022/problem2.js
@@ -8,6 +8,15 @@ Example(Input --> Output):
 
 */
 
+// P: parameters: only strings that could have any characters
+// R: returns: reverses all the words in the string
+// E: examples: "The greatest victory is that which requires no battle" -> "battle no requires which that is victory greatest The"
+// P: pseudocode
+
 function reverseWords(str){
-    return str; // reverse those words
+    // convert string into array
+    // break the array elements split by space
+    // reverse it
+    // join it 
+    return str.split(" ").reverse().join(" ");
   }

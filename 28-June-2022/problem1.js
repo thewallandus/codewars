578f65603be7bdd5e3e2f18c59af58cd32b7572e
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-28 11:45:06 +0530

    problem 1 pseudocode V1

--- 28-June-2022/problem1.js
+++ 28-June-2022/problem1.js
@@ -3,3 +3,15 @@
 This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 */
+
+// P: parameters: a number
+// R: multiply a given number by 8 if even or 9 otherwise
+// E: 2, 8
+// P: pseudo code
+
+function simpleMultiplication(number) {
+    // run a conditional to check even or odd
+    // if even multiply by 8
+    // Else 9
+
+}

52c38c1540c65ec206b1eb307fb4a855e98088a5
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-28 11:55:51 +0530

    problem 2 solution

--- 28-June-2022/problem2.js
+++ 28-June-2022/problem2.js
@@ -11,3 +11,28 @@ Note:
 Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 
 */
+
+// P: receive an array with class test scores, and your score
+// R: return either true or false
+// E: [2, 3], 5
+// P: pseudocode
+
+
+function betterThanAverage(classPoints, yourPoints) {
+    // first need an iterator that gives the sum of everything
+    // Then it stores the sum in a variable
+    // Then add this variable to yourpoints
+    // then run a contional 
+		let sum = classPoints.reduce((element, accumulator) => {
+      return element + accumulator
+    }, 0)  
+		let average = (sum + yourPoints) / (classPoints.length + 1);
+  	if (yourPoints > average) {
+      return true
+    } else {
+      return false
+    }
+}
+  
+betterThanAverage([2, 3], 5)
+betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75);

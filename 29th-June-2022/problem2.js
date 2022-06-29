1455d72d293e08640e1369338a781f285d790fa6
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-29 12:17:56 +0530

    problem 2 solution

--- 29th-June-2022/problem2.js
+++ 29th-June-2022/problem2.js
@@ -6,3 +6,16 @@ P.S. Each array includes only integer numbers. Output is a number too.
 
 */
 
+
+function arrayPlusArray(arr1, arr2) {
+    let sumArr1 = arr1.reduce((el, accumulator) => {
+      return el + accumulator
+    })
+    
+    let sumArr2 = arr2.reduce((element, acc) => {
+      return element + acc
+    })
+    
+    return sumArr1 + sumArr2
+     
+   }

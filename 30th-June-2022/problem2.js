895581ce844583e2d4733994083ab511e3a5e093
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-06-30 16:21:54 +0530

    problem 2 solution

--- 30th-June-2022/problem2.js
+++ 30th-June-2022/problem2.js
@@ -9,3 +9,21 @@ Examples(Input1, Input2 --> Output):
 "paper", "paper" --> "Draw!"
 
 */
+
+// P: option1, option2 as strings
+// R: return string
+// E: 'rock', 'scissors'. 'scissors', 'paper'.
+// P: pseudo-code
+
+const rps = (p1, p2) => {
+    // p1 and p2 are string with the options
+    // need a conditional: switch case statement
+    // return the result
+		if (p1 === p2) {
+      return "Draw!"
+    } else if (p1 === 'rock' && p2 === 'scissors' || p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock') {
+      return "Player 1 won!"
+    } else if (p2 === 'rock' && p1 === 'scissors' || p2 === 'scissors' && p1 === 'paper' || p2 === 'paper' && p1 === 'rock') {
+      return "Player 2 won!"
+    }
+};

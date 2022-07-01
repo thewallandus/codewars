b649f9ba7861f9f958c21edb88a79cf641782986
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-01 09:52:44 +0530

    problem 2 7kyu first try

--- 1st-July-2022/problem2.js
+++ 1st-July-2022/problem2.js
@@ -7,3 +7,23 @@ For example, if we run 9119 through the function, 811181 will come out, because 
 Note: The function accepts an integer and returns an integer
 
 */
+
+// P: is an integer number 
+// R: is an integer number
+// E: squareDigits(3212), 9414 AND squareDigits(2112), 4114
+// P: pseudo-code
+
+
+function squareDigits(num){
+    // I take in an integer
+    // then create a new variable that turns it into a string
+    // Then run it through a for loop 
+    // new empty variable += "NUM(str[i]) ** 2"
+    // return NUM of the new string variable
+		let newNum = num.toString();
+  	let str = "";
+  	for (let i = 0; i < newNum.length; i++) {
+      str += Number(newNum[i]) ** 2
+    }
+  	return Number(str)
+}

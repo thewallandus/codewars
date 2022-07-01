8e150eff85eab783218b21bdeaa5657aba9b8fa8
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-01 09:28:50 +0530

    problem 1 solution

--- 1st-July-2022/problem1.js
+++ 1st-July-2022/problem1.js
@@ -7,6 +7,22 @@ Given a non-negative integer, 3 for example, return a string with a murmur: "1 s
 
 */
 
+// P: number that is an integer and 0
+// R: either empty string or num + sheep...
+// E: countSheep(0), "" || (countSheep(1), "1 sheep...")
+// P: pseudo-code
+
+
 let countSheep = function (num){
-    //your code here
-  }
+    // create an empty string
+    // do concatenation num times using a simple loop    
+  	let str = "";
+  	if (num === 0) {
+      return str
+    } else if (num > 0) {
+      	for (let i = 1; i <= num; i++) {
+      	str += `${i} sheep...`
+    	}
+    }
+  	return str;
+}

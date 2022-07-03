f06760a996bfd532941b103b30f34d8d09f7228f
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-03 16:27:10 +0530

    problem 2 question

---
+++ 3rd-July-2022/problem2.js
@@ -0,0 +1,14 @@
+/*
+
+Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+
+*/
+
+// P: parameters
+// R: returns
+// E: examples
+// P: pseudocode
+
+String.prototype.digit = function() {
+    return false;
+  };

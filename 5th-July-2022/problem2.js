b7e121007d6a9924e228a48c9cf89096e14e0d5d
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-05 18:45:19 +0530

    problem 2 solution first try

--- 5th-July-2022/problem2.js
+++ 5th-July-2022/problem2.js
@@ -17,6 +17,27 @@ there are 10 matches in the championship
 
 */
 
+// P: parameters: an array of strings. 
+// R: total points
+// E: ["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]
+// P: pseudo-code
+
+
 function points(games) {
-    // your code here
+    // create a variable totalscore of X 
+   // wrap a for loop with total 10 matches
+  // and 3rd character of the array's element
+    // and 1st character of the array's element
+  // if condition met change the score
+		let score = 0;
+  	for (let i = 0; i < games.length; i++) {
+      if (Number(games[i][0]) > Number(games[i][2])) {
+        score += 3
+      } else if (Number(games[i][0]) < Number(games[i][2])) {
+        score += 0
+      } else if (Number(games[i][0]) === Number(games[i][2])) {
+        score += 1
+      }
+    }
+  	return score
   }

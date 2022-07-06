742ba1b65732d3a763f9e00ce507a7c981f66983
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-06 16:57:53 +0530

    problem 2 solution

--- 6th-July-2022/problem2.js
+++ 6th-July-2022/problem2.js
@@ -22,7 +22,31 @@ The middle character(s) of the word represented as a string.
 
 */
 
-function getMiddle(s)
-{
-  //Code goes here!
+// P: parameters: it's a string that can be odd or even characters
+// R: returns: a string that is 1 or 2 characters depending on conditional
+// E: Kata.getMiddle("test") should return "es"
+// P: pseudo-code
+
+
+function getMiddle(s) {
+	// create a conditional
+  // if s.length % 2 === 0 => return the middle two characters
+  //if s.length % 2 != 0 => return the middle character 
+  // how do you calculate the middle two characters => 4 || 0, 1, 2, 3 
+  //"testing" || 0, 1, 2, 3, 4, 5, 6 => s.length - 1 / 2
+  //"middle" || 0, 1, 2, 3, 4, 5 => s.length - 1 / 2 
+  // create a variable for s.length - 1 / 2
+  // Math.floor it for the lower number
+  // Math.ceil it for the upper number
+  // For the odd one it should still work
+  let mid = (s.length - 1) / 2;
+  console.log(mid)
+  let ret = "";
+  if (s.length % 2 === 0) {
+    ret += s[Math.floor(mid)] + s[Math.ceil(mid)]
+    console.log(ret)
+  } else if (s.length % 2 != 0) {
+    ret += s[mid]
+  }
+  return ret;
 }

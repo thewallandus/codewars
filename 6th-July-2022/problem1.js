6c30bf146bf78032074a86f48effc1ca33a911b9
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-06 16:16:50 +0530

    problem 1 solution

--- 6th-July-2022/problem1.js
+++ 6th-July-2022/problem1.js
@@ -13,6 +13,22 @@ Output string must be two numbers separated by a single space, and highest numbe
 
 */
 
+//P: parameters: string with a bunch of numbers
+//R: a string with 2 numbers and 1 space in between
+//E:highAndLow("1 2 3 4 5");  // return "5 1"
+//highAndLow("1 2 -3 4 5"); // return "5 -3"
+//P: pseudo-code
+
 function highAndLow(numbers){
-    // ...
+    //need to convert numbers into array
+    //create a variable for minimum number
+    //create a variable for maximum number
+    //create a variable with an empty string
+    // add the min var and max var with a template literal
+    // return the new string
+  	let arr = numbers.split(" ")
+  	let min = Math.min(...arr)
+    let max = Math.max(...arr)
+  	let newString = `${max} ${min}`
+    return newString
 }

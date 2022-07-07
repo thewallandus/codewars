6a7367c79fbc2ea515c9388aa7aba0b57ce4af87
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-07 17:08:00 +0530

    problem 1 solution

--- 7th-July-2022/problem1.js
+++ 7th-July-2022/problem1.js
@@ -5,3 +5,16 @@ The main idea is to count all the occurring characters in a string. If you have 
 What if the string is empty? Then the result should be empty object literal, {}.
 
 */
+
+// P: either an empty string or a string || string
+// R: returning an object || and empty object
+// E: count("aba"), { a: 2, b: 1 }
+// P: 
+
+function count (string) {  
+    const count = {}
+    string.split('').forEach((character) => {
+      count[character] = count[character] ? (count[character] + 1) : 1
+    })
+    return count
+  }

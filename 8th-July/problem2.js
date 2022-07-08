887c6bbb3caa8b72e18a0e55b46d3f1154803711
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-08 09:11:21 +0530

    problem 2 solution

--- 8th-July/problem2.js
+++ 8th-July/problem2.js
@@ -9,6 +9,16 @@ filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 */
 
+
 function filter_list(l) {
-    // Return a new array with the strings filtered out
+	let newl = [];
+  for (let i = 0; i < l.length; i++) {
+    if (typeof l[i] === 'number') {
+      newl.push(l[i])
+    }
+  }
+  return newl
 }
+
+filter_list([1, 2, 'a', 'b'])
+filter_list([1, 'a', 'b', 0, 15])

1f20dd6b0871cb3126661efbb7314a7602a3a741
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-10 09:56:54 +0530

    problem 2 question

--- 10th-July-2022/problem2.js
+++ 10th-July-2022/problem2.js
@@ -12,6 +12,13 @@ Link to Jaden's former Twitter account @officialjaden via archive.org
 
 */
 
+//P: parameters: string
+//R: returns: string
+// //E: examples: 
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+//P: pseudo-code
+
 function jadenCase(string) {
   
 }

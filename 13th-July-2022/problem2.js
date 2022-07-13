81379521651b68bdb560a8eff86444884f5c5a5f
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-13 21:33:37 +0530

    problem 2 PREP

--- 13th-July-2022/problem2.js
+++ 13th-July-2022/problem2.js
@@ -6,6 +6,11 @@ String will never be empty and you do not need to account for different data typ
 
 */
 
-function findShort(s){
+// P: long string with multiple characters
+// R: return the length of the shortest word || ie a number
+// E: findShort("bitcoin take over the world maybe who knows perhaps"), 3
+// P: pseudo-code
 
+function findShort(s){
+    
 }

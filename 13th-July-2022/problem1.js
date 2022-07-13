60e3903df9492a9263474a5916834bb3d50b92fa
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-13 21:09:35 +0530

    problem 1 question

---
+++ 13th-July-2022/problem1.js
@@ -0,0 +1,26 @@
+/*
+
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+
+*/
+
+// P: string with any number of characters
+// R: a boolean that's true or false
+// // E: XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+// P: pseudo-code
+
+function XO(str) {
+    //code here
+}

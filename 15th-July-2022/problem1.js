200239b9fad67e29d4b5d8e6be93b6aba2cfa665
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-15 17:30:36 +0530

    problem 1 solution

--- 15th-July-2022/problem1.js
+++ 15th-July-2022/problem1.js
@@ -8,6 +8,20 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 
 */
 
+//P: an array with minimum 4 positive integers
+//R: sum of the two lowest positive numbers
+//E: [19, 5, 42, 2, 77], the output should be 7.
+//P: pseudo-code
+
+
 function sumTwoSmallestNumbers(numbers) {  
-    
+    //find the smallest number and push it out of the array using splice (once you have the index)
+    // Also save it in a variable
+    // find the smallest number in the new array
+    // save it in a variable
+    // return the sum of the two variables
+  	const min1 = Math.min(...numbers)
+    numbers.splice(numbers.indexOf(min1), 1)
+  	const min2 = Math.min(...numbers)
+  	return min1 + min2
 }

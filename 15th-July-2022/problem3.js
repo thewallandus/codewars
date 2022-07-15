e8957ca070b4081aa0b2c9e286edcc1859d0febc
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-15 17:45:31 +0530

    problem 3 solution PREP

--- 15th-July-2022/problem3.js
+++ 15th-July-2022/problem3.js
@@ -13,3 +13,14 @@ Input validation
 If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
 
 */
+
+//P: parameters: an array
+//R: return a sum [integer] except highest and lowest number
+//E: { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+//P: pseudo-code
+
+
+function sumArray(array) {
+
+}

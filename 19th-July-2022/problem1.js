3f44f33225380fd13ae92e37d577aa3a64599143
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-19 11:53:13 +0530

    Question 1 PREP

--- 19th-July-2022/problem1.js
+++ 19th-July-2022/problem1.js
@@ -10,18 +10,12 @@ If the whole array is consecutive then return null2.
 
 The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
 
-If you like this Kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
-
-1 Can you write a solution that will return null2 for both [] and [ x ] though? (This is an empty array and one with a single number and is not tested for, but you can write your own example test. )
-
-2
-Swift, Ruby and Crystal: nil
-Haskell: Nothing
-Python, Rust, Scala: None
-Julia: nothing
-Nim: none(int) (See options)
-
 */
+
+// P: an array of numbers
+// R: the first non-consecutive number
+// E: firstNonConsecutive([1,2,3,4,6,7,8]) => 6
+// P: pseudo-code
 
 function firstNonConsecutive (arr) {
 

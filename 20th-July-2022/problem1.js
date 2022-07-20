612172913c52810b0d7ea255e4539d52b7218976
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-20 09:48:02 +0530

    problem 1 solution 3rd try

--- 20th-July-2022/problem1.js
+++ 20th-July-2022/problem1.js
@@ -16,5 +16,9 @@ Write a function that when given a URL as a string, parses out just the domain n
 // P: pseudo-code
 
 function domainName(url){
-    //your code here
+    
+    let sourceString = url.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];	
+    let domain = sourceString.split(".")[0];
+    return domain;
+
 }

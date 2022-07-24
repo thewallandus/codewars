0d5858fdfe988bb1987d788ed7315921b78c6102
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-24 15:29:09 +0530

    problem 2 PREP

--- 24th-July-2022/problem2.js
+++ 24th-July-2022/problem2.js
@@ -9,6 +9,12 @@ None of the arrays will be empty, so you don't have to worry about that!
 
 */
 
+// P: an array of ANYTHING
+// R: return an array
+// // E: 
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+// P: pseudo-code
+
 function removeEveryOther(arr){
     //your code here
   }

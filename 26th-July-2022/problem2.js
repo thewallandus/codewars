5e01292104ec39a9e5ece8497ba42e7ef39121bc
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-26 15:11:37 +0530

    problem 2 solution first try

--- 26th-July-2022/problem2.js
+++ 26th-July-2022/problem2.js
@@ -8,9 +8,9 @@ Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his 
 function createArray(number){
     var newArray = [];
     
-    for(var counter = 1; counter <= number;){
+    for(var counter = 1; counter <= number; counter++){
       newArray.push(counter);
     }
     
     return newArray;
-}
+  }

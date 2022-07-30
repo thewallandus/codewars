8cde06cba19e38bda9b51b6ab4f343b0134a0116
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-30 16:45:53 +0530

    problem 1 solution first try

--- 30th-July-2022/problem1.js
+++ 30th-July-2022/problem1.js
@@ -36,5 +36,13 @@ Examples(Inputs-->Output):
 // P: pseudo-code
 
 function finalGrade (exam, projects) {
-    return // final grade
-  }
+    return exam > 90 && projects > 10 ? 100
+        :  exam > 75 && projects >= 5 ? 90
+        :  exam > 50 && projects >= 2 ? 75 : 0
+  
+    // 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+    // 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+    // 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+    // 0, in other cases
+
+}

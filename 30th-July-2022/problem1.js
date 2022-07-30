d968cde990d83c016c7ebb42c23cc269ade3e41a
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-07-30 16:54:06 +0530

    problem 1 solution 2nd try

--- 30th-July-2022/problem1.js
+++ 30th-July-2022/problem1.js
@@ -36,7 +36,7 @@ Examples(Inputs-->Output):
 // P: pseudo-code
 
 function finalGrade (exam, projects) {
-    return exam > 90 && projects > 10 ? 100
+    return exam > 90 || projects > 10 ? 100
         :  exam > 75 && projects >= 5 ? 90
         :  exam > 50 && projects >= 2 ? 75 : 0
   

d242f6143c35425036ebcc2c9c7656d27ad8a55a
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-02 19:24:40 +0530

    fixed conditional

--- 2nd-August-2022/problem1.js
+++ 2nd-August-2022/problem1.js
@@ -11,7 +11,7 @@ function findLongest(str) {
     var longest = 0
     
     for (var i = 0; i > spl.length; i++) {
-      if (spl(i).length > longest) {
+      if (spl[i].length > longest) {
         longest = spl[i].length
     }
       }

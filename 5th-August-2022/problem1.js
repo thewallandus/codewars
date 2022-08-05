c1c4264a92ec8f2a845f53d7c02dedccd3e1b8a3
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-05 17:55:02 +0530

    debug 6

--- 5th-August-2022/problem1.js
+++ 5th-August-2022/problem1.js
@@ -13,7 +13,7 @@ Good luck!
 function howManyDalmations(number) {
 
   
-    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
     
     var respond = number <= 10 ? dogs[0]
                 : number <= 50 ? dogs[1] 

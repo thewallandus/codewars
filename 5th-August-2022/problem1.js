7651a09e149d611f75aa07a04ad041c784f02f5d
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-05 17:45:41 +0530

    problem 1 question update

--- 5th-August-2022/problem1.js
+++ 5th-August-2022/problem1.js
@@ -9,3 +9,12 @@ The number of dogs will always be a number and there will always be at least 1 d
 Good luck!
 
 */
+
+function howManyDalmations(numer :
+  
+    var dogs ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    
+    var respond = number <= 10 ? dogs[0] (number <= 50 ? dogs[1] : (number = 101  dogs[3] : dogs[2]
+    
+  return respond
+  

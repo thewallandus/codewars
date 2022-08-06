adb9fcc71f8e4f7242519023e4dc76e08c16821f
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-06 17:04:05 +0530

    problem 1 question update

--- 6th-August-2022/problem1.js
+++ 6th-August-2022/problem1.js
@@ -4,4 +4,16 @@ You probably know the "like" system from Facebook and other pages. People can "l
 
 Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
 
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+
 */
+
+function likes(names) {
+    // TODO
+  }

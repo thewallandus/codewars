e603633579d2fa2a2494aee2f4422d1b13e92168
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-07 12:15:40 +0530

    problem 1 solution

--- 7th-August-2022/problem1.js
+++ 7th-August-2022/problem1.js
@@ -18,6 +18,24 @@ Example: (Input --> Output)
 // "moOse" --> false (ignore letter case)
 // P
 
-function isIsogram(str){
-    //...
+function isIsogram(str) {
+    //if empty return true.
+    if (str.isEmpty) {
+      return true;
+    } else {
+      // All lower case.
+      str = str.toLowerCase();
+    }
+    //split string into individual characters. 
+    var array = str.split('');
+    var sortedArr = array.slice().sort();
+  
+    for (var i = 0; i < array.length; i++) {
+      //if duplicate is found return false.
+      if (sortedArr[i + 1] == sortedArr[i]) {
+        return false;
+      }
+    }
+    //else return true
+    return true;
   }

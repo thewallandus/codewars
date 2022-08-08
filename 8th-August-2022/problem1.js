11749d8c4ab5dfd0eda0a7372ead268d3c0dfcf9
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-08 18:06:03 +0530

    debug 4

--- 8th-August-2022/problem1.js
+++ 8th-August-2022/problem1.js
@@ -14,7 +14,7 @@ Remember that typically temperatures in the current weather conditions are given
 function weatherInfo (temp) {
     var c = convertToCelsius(temp)
     if (c < 0)
-      return (c + " is freezing temperature")
+      return `${c} is freezing temperature`
     else
       return (c + " is above freezing temperature")
   }

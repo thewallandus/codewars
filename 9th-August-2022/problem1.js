7316493efd088ac908cc2b67170764af9212c22e
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-09 21:26:54 +0530

    problem 1 PREP

--- 9th-August-2022/problem1.js
+++ 9th-August-2022/problem1.js
@@ -4,4 +4,9 @@ This method, which is supposed to return the result of dividing its first argume
 
 */
 
+// P: 2 numbers
+// R: result of division of number 1 by number 2
+// E: 5 / 9
+// P: 
+
 const solve = (x, y) => x // y

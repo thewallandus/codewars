182b536a5c5ca8ffd97a8e07cfa4ceff57658ec4
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-09 21:18:05 +0530

    problem 1 question

--- 9th-August-2022/problem1.js
+++ 9th-August-2022/problem1.js
@@ -5,3 +5,10 @@ I would like to be able to pass an array with two elements to my swapValues func
 Can you figure out what's wrong here?
 
 */
+
+function swapValues() {
+    var args = Array.prototype.slice.call(arguments);
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+}

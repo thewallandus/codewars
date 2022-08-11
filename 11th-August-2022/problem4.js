a2655ac3cd19d6b3b8ac6cfb986f92c90235a041
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-11 11:56:16 +0530

    problem 4 solution

--- 11th-August-2022/problem4.js
+++ 11th-August-2022/problem4.js
@@ -14,7 +14,22 @@ divisors(13); // should return "13 is prime"
 // if number is prime => return (integer) is prime
 
 function divisors(integer) {
-  
-};
+    // if number divisible by itself and 1 => prime
+    // create an array of the divisible numbers
+    // by default they have 1 and the number itself as the start and end value
+    // so start a for loop and push all the nums it is divisible by and set the start and end value as 1 and the integer
+    // finally check with an if else statement if arr.length === 2 else sort it and remove the first and last value and return it
+    let arr = [1, integer]
+      for (let i = 2; i < integer; i++) {
+      if (integer % i === 0) {
+        arr.push(i)
+      }
+    }
+    if (arr.length === 2) {
+         return `${integer} is prime` 
+    } else {
+       return arr.sort((a,b) => a - b).slice(1, -1)
+    }
+  };
 
 

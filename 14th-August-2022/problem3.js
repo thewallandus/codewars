cdde52a6e7a0c6acb659a6f4331a7eb2b7318fe9
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-14 11:27:19 +0530

    problem 3 solution 2nd try

--- 14th-August-2022/problem3.js
+++ 14th-August-2022/problem3.js
@@ -9,7 +9,8 @@ array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 
 */
 
+
 function index(array, n){
-    return n > array.length ? -1 
+    return n > array.length - 1  ? -1 
         : array[n] ** n    
 }

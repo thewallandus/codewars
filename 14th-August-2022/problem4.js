61c57354f0b18023118a0f04c6a9ecc317983d08
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-14 11:33:05 +0530

    problem 4 solution 2nd try

--- 14th-August-2022/problem4.js
+++ 14th-August-2022/problem4.js
@@ -10,8 +10,8 @@ Example(Input1, Input2 --> Output)
 function divisibleBy(numbers, divisor){
     let arr = []
     for (let i = 0; i < numbers.length; i++) {
-        if (numbers[0] % divisor === 0) {
-            arr.push(numbers[0])
+        if (numbers[i] % divisor === 0) {
+            arr.push(numbers[i])
         }
     }
     return arr

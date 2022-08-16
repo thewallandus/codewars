434f6096a4561c4aedae9314ceb7f602231b3659
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-16 16:44:33 +0530

    problem 2 pseudo-code attempt 1

--- 16th-August-2022/problem2.js
+++ 16th-August-2022/problem2.js
@@ -5,5 +5,13 @@ Write a function that checks if a given string (case insensitive) is a palindrom
 */
 
 function isPalindrome(x) {
-    // your code here
-  }
+    
+    // use slice and create a new string
+    // save it in a variable
+    // turn it into an array
+    // then reverse it
+    // then join it
+    // then check if the string is equal to the new string
+    // if yes => then true
+    // else false
+}

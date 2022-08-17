e9e274e10fc1b6b5f72312aabd15ef5ff2a465aa
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-17 21:57:32 +0530

    problem 1 solution

--- 17th-August-2022/problem1.js
+++ 17th-August-2022/problem1.js
@@ -28,5 +28,8 @@ Examples: (Input --> Output)
 // P
 
 function peopleWithAgeDrink(old) {
-    
-};
+    return old < 14 ? "drink toddy" 
+        : old < 18 ? "drink coke"
+        : old < 21 ? "drink beer"
+        : "drink whisky"
+}

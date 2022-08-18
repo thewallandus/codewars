f0d30b782c8c311018953501041b788e90b86e48
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-18 09:09:21 +0530

    problem 1 question

--- 18th-August-2022/problem1.js
+++ 18th-August-2022/problem1.js
@@ -14,3 +14,9 @@ Your function would return the following array:
 The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 
 */
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    
+    // return an array containing all of the strings in the input array except those that match strings in geese
+  };

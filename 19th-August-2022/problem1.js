42fc7340f2682824c542623a4264f3cd3f3e40c6
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-19 11:04:35 +0530

    problem 1 solution refactored

--- 19th-August-2022/problem1.js
+++ 19th-August-2022/problem1.js
@@ -28,6 +28,6 @@ function isPythagoreanTriple(integers) {
     // square of the max integer => sum of the square of the other two 
     // then return true
     // or else return false
-    const ascend = integers.sort((a, b) => return a-b);
+    const ascend = integers.sort((a, b) => a-b);
     return (ascend[2] ** 2) === ((ascend[0] ** 2) + (ascend[1] ** 2)) ? true : false
   }

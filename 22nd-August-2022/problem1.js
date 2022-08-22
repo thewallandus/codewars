745d34ecfd8f05e2f2d66b327ab5ee731f42fc9c
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-22 22:05:08 +0530

    problem 1 question

--- 22nd-August-2022/problem1.js
+++ 22nd-August-2022/problem1.js
@@ -17,3 +17,7 @@ For example, a multiplication table (string) for number == 5 looks like below:
 P. S. You can use \n in string to jump to the next line.
 
 */
+
+function multiTable(number) {
+    // good luck
+  }

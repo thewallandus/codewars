9ea818f4e4919486bb7076bfcf835c4bf12e99f2
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-28 09:26:12 +0530

    problem 2 question

---
+++ 28th-August-2022/problem2.js
@@ -0,0 +1,10 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// PROBLEM 2 SOLUTION FORMULA:
+
+// 9 = 9 * (16 ^ 0) = 9
+// C = 12 * (16 ^ 1) = 192
+
+function hexToDec(hexString){
+    //your code here
+  }

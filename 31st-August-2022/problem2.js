f179c9f49991c344b6d68370dee1ee807a0bb7dd
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-31 08:44:36 +0530

    problem 2 solution

--- 31st-August-2022/problem2.js
+++ 31st-August-2022/problem2.js
@@ -10,5 +10,6 @@ function isPalindrome(x) {
     // turn string into lowercase
     // split then reverse it and see if it's equal to the orinal string in lower case
     // if yes => then true
-    // if no => then false    
-}
+    // if no => then false
+    return x.toLowerCase() === x.toLowerCase().split('').reverse().join('') ? true : false
+      }

acca9328eddb37b859fc144ceeceb487f60d7085
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-31 08:52:12 +0530

    problem 4 fisrt try solution

--- 31st-August-2022/problem4.js
+++ 31st-August-2022/problem4.js
@@ -7,3 +7,7 @@ If you need help, here's a reference:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 
 */
+
+function take(arr, n) {
+    return arr.slice(0, n)
+  }

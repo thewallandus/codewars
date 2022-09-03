cba50d2464625d9820f3da32a67f3c607a0031da
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-03 08:41:26 +0530

    problem 1

--- 3rd-September-2022/problem1.js
+++ 3rd-September-2022/problem1.js
@@ -14,3 +14,7 @@ When you have finished the work, click "Run Tests" to see if your code is workin
 In the end, click "Submit" to submit your code pass this kata.
 
 */
+
+function saleHotdogs(n){
+    return ?
+  }

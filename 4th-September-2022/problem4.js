dad09072ff9355a12702f98f094f50dc52f421c7
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-04 12:13:10 +0530

    problem 4 solution

--- 4th-September-2022/problem4.js
+++ 4th-September-2022/problem4.js
@@ -18,6 +18,13 @@ Examples
 // [17, 17, 3, 17, 17, 17, 17] ==> 3
 // p
 
+
+
 function stray(numbers) {
-    
-}
+    let arr = numbers.sort();
+    if (arr[0] !== arr[1]) {
+      return arr[0];
+    } else {
+      return arr[arr.length - 1];
+    }
+  }

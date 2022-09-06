bf57e036b381ecaf532ddf535cf8d8382cad93dd
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-06 20:59:08 +0530

    problem 1

--- 6th-September-2022/problem1.js
+++ 6th-September-2022/problem1.js
@@ -4,3 +4,18 @@ Our counter prototype is broken. Can you spot, what's wrong here?
 
 */
 
+function Counter() {
+    this.value = 0;
+  }
+  
+  Counter.prototype.increase() {
+    this.value++;
+  };
+  
+  Counter.prototype.getValue() {
+    return this.value;
+  };
+  
+  Counter.prototype.reset() {
+    this.value = 0;
+  };

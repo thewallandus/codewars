23a47df5ce454ddff9105f38aebd15cd6bc7da91
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-06 21:04:13 +0530

    problem 1 solution

--- 6th-September-2022/problem1.js
+++ 6th-September-2022/problem1.js
@@ -8,14 +8,16 @@ function Counter() {
     this.value = 0;
   }
   
-  Counter.prototype.increase() {
+  Counter.prototype.increase = () => {
     this.value++;
   };
   
-  Counter.prototype.getValue() {
+  Counter.prototype.getValue = () => {
     return this.value;
   };
   
-  Counter.prototype.reset() {
+  Counter.prototype.reset = () => {
     this.value = 0;
   };
+
+

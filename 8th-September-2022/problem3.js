7f8b155c086a4e461ae8325b48d04534ed45d319
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-08 12:09:10 +0530

    problem 3 solution

--- 8th-September-2022/problem3.js
+++ 8th-September-2022/problem3.js
@@ -13,7 +13,6 @@ checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
 checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
 
 */
-
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
-  
-}
+    return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+  }

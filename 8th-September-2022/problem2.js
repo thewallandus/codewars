8059c03f06ff30fe76fbfbcc70016f67299000b8
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-08 11:08:01 +0530

    problem 2 question

--- 8th-September-2022/problem2.js
+++ 8th-September-2022/problem2.js
@@ -5,3 +5,7 @@ Write a small function that returns the values of an array that are not odd.
 All values in the array will be integers. Return the good values in the order they are given.
 
 */
+
+function noOdds( values ){
+    // Return all non-odd values
+  }

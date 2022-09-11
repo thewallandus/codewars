0de0bdaa0a659d02b7ab422095dabffe2e6cd978
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-11 16:49:22 +0530

    problem 9 solution

--- 11th-September-2022/problem9.js
+++ 11th-September-2022/problem9.js
@@ -4,7 +4,7 @@ Fix the variables assigments so that this code stores the string 'devLab' in the
 
 */
 
-var a == "dev"
-var b == "Lab"
+var a = "dev"
+var b = "Lab"
 
-var name == a + b
+var name = a + b

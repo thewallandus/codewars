b3132870df1e1692ea843c8f08cea56d0eac6f3c
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-11 16:36:43 +0530

    problem 7 solution

--- 11th-September-2022/problem7.js
+++ 11th-September-2022/problem7.js
@@ -15,5 +15,14 @@ Zero alone is fine, don't worry about it. Poor guy anyway
 */
 
 function noBoringZeros(n) {
-    // your code
-  }
+    let str = n.toString().split('');
+
+    for (let i = 0; i <=str.length; i++){
+        if(str[str.length-1] == '0'){
+
+            str.pop();
+        }
+    }
+
+    return +str.join('')
+}

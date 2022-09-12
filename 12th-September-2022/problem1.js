f86188ad4ed4bee11d06c23cfa385c98c6079c5b
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-12 10:25:50 +0530

    problem 1 solution

--- 12th-September-2022/problem1.js
+++ 12th-September-2022/problem1.js
@@ -7,10 +7,9 @@ Your task is to debug the code before your plants die!
 */
 
 function rainAmount(mm){
-    if (rainAmount = 40) {
-         return "You need to give your plant " + {rainAmount - 40} + " mm of water"
-    };
-    if else {
+    if (mm < 40) {
+         return "You need to give your plant " + `${40 - mm}` + "mm of water"
+    } else {
          return "Your plant has had more than enough water for today!"
-    };
+    }
 }

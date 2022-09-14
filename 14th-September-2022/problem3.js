2c96cf4766f94b8b917d8f3b3454e6f1b5f7b714
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-14 11:20:27 +0530

    problem 3 solution

--- 14th-September-2022/problem3.js
+++ 14th-September-2022/problem3.js
@@ -8,25 +8,14 @@ The inputs x and y are always greater or equal to 1, so the greatest common divi
 
 
 function mygcd(x,y){
-    const xArr = []
-    const yArr = []
-    for (let i = 1; i <= x; i++) {
-        if (x % i === 0) {
-            xArr.push(i)
-        }
-    }
-    for (let j = 1; j <= x; j++) {
-        if (y % j === 0) {
-            yArr.push(j)
-        }
-    }
-  	const xAndY = []
-    for (let a = 0; a < xArr.length; a++) {
-      for (let b = 0; b < yArr.length; b++) {
-        if (xArr[a] === yArr[b]) {
-          xAndY.push(yArr[b])
-        }
+    let gcf = 0
+    for (let i = 1; i <= x && i <= y; i++) {
+  
+      // check if is factor of both integers
+      if( x % i === 0 && y % i === 0) {
+         gcf = i
       }
+  
     }
-  return Math.max(...xAndY)
-}
+    return gcf
+  }

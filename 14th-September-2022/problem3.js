846c469ec3b75e0b3eaf555e7a8ff676ce1bbd3b
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-14 11:02:04 +0530

    problem 3 solution 2nd part - yet to be fixed

--- 14th-September-2022/problem3.js
+++ 14th-September-2022/problem3.js
@@ -6,6 +6,7 @@ The inputs x and y are always greater or equal to 1, so the greatest common divi
 
 */
 
+
 function mygcd(x,y){
     const xArr = []
     const yArr = []
@@ -19,4 +20,13 @@ function mygcd(x,y){
             yArr.push(j)
         }
     }
+  	const xAndY = []
+    for (let a = 0; a < xArr.length; a++) {
+      for (let b = 0; b < yArr.length; b++) {
+        if (xArr[a] === yArr[b]) {
+          xAndY.push(yArr[b])
+        }
+      }
+    }
+  return Math.max(...xAndY)
 }

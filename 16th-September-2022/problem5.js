7a03be3fed61dc9735311a645c9b4219de710323
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-15 11:58:36 +0530

    problem 5 solution

---
+++ 16th-September-2022/problem5.js
@@ -0,0 +1,22 @@
+/*
+
+Complete the method which accepts an array of integers, and returns one of the following:
+
+"yes, ascending" - if the numbers in the array are sorted in an ascending order
+"yes, descending" - if the numbers in the array are sorted in a descending order
+"no" - otherwise
+You can assume the array will always be valid, and there will always be one correct answer.
+
+*/
+const isSortedAndHow = (array) => {
+    const sortedArray = array.slice().sort((a, b) => a - b);
+    
+    if (JSON.stringify(sortedArray) === JSON.stringify(array)) {
+      return 'yes, ascending';
+    }
+    if (JSON.stringify(sortedArray.reverse()) === JSON.stringify(array)) {
+      return 'yes, descending';
+    }
+    
+    return 'no';
+  };

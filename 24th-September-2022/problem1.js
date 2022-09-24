4b244679887a3a48a817e78ae9a189e027006435
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-24 07:17:40 +0530

    problem 1 solution

--- 24th-September-2022/problem1.js
+++ 24th-September-2022/problem1.js
@@ -23,7 +23,7 @@ In the end, click "Submit" to submit your code pass this kata.
 */
 
 function animal(obj){
-    return ?
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
   }
   
   

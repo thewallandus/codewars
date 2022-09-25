19c060eaa7c1600cfff9853f908a7565f345f05a
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-25 09:18:07 +0530

    problem 3 solution

--- 25th-September-2022/problem3.js
+++ 25th-September-2022/problem3.js
@@ -11,7 +11,12 @@ remove("Hi! Hi!") === "Hi! Hi"
 remove("Hi") === "Hi"
 
 */
-
-function remove (string) {  
-    return '';
-}
+function remove(s){
+    let arr =[]
+      for (let i = s.length-1; i => 0; i--){
+        if(s[i] != '!'){
+          arr.push(s.slice(0,i+1));
+          return arr.join('');
+        }  
+      }
+    }

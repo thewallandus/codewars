b8a903e29a20a7b6881cf0a1b8f2ddd2fd147e3f
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-25 09:00:14 +0530

    problem 1 question

---
+++ 25th-September-2022/problem1.js
@@ -0,0 +1,45 @@
+/*
+
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+
+*/
+
+function add(a,b){
+    return 
+}
+
+function divide(a,b){
+    return 
+}
+
+function multiply(a,b){
+    return 
+}
+
+function mod(a,b){
+    return 
+}
+   
+function exponent(a,b){
+    return 
+}
+    
+function subt(a,b){
+    return 
+}

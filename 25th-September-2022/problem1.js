d35e77b423a8482d97843af0ca27a2d49ba11fed
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-25 09:01:04 +0530

    problem 1 solution

--- 25th-September-2022/problem1.js
+++ 25th-September-2022/problem1.js
@@ -19,27 +19,26 @@ subtraction = subt
 Note: All math operations will be: a (operation) b
 
 */
-
 function add(a,b){
-    return 
+    return a + b
 }
 
 function divide(a,b){
-    return 
+    return a / b
 }
 
 function multiply(a,b){
-    return 
+    return a * b
 }
 
 function mod(a,b){
-    return 
+    return a % b
 }
    
 function exponent(a,b){
-    return 
+    return a ** b
 }
     
 function subt(a,b){
-    return 
+    return a - b
 }

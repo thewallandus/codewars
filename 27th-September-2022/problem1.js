ed87c0fab8424cb57c1d4c559046e839473f32da
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-27 08:54:20 +0530

    problem 1 solution 2

--- 27th-September-2022/problem1.js
+++ 27th-September-2022/problem1.js
@@ -15,8 +15,12 @@ sumMul(4, -7)  ==> "INVALID"
 
 function sumMul(n,m){
     let sum = 0
-    for (let i = n; i < m; i+=n) {
+    if (n <= 0 || m <= 0) {
+      return "INVALID"
+    } else {
+      for (let i = n; i < m; i+=n) {
         sum += i
+      }  
     }
     return sum
 }

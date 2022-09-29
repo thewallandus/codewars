63ca426173495ff049a5682f5739505e5f8da371
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-29 07:33:17 +0530

    problem 2 solution

--- 29th-September-2022/problem2.js
+++ 29th-September-2022/problem2.js
@@ -9,5 +9,5 @@ Time to test your basic knowledge in functions! Return the odds from a list:
 
 function odds(values){
     // arrow it
-    return values.filter( => );
+    return values.filter(item => item % 2 === 1);
   }

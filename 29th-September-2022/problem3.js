e0155072b8f519c0f799cb4a62c4e59f1cb799a5
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-09-29 07:51:58 +0530

    problem 3 solution

--- 29th-September-2022/problem3.js
+++ 29th-September-2022/problem3.js
@@ -13,5 +13,8 @@ if it was over 88 you should return "Leo got one already!"
 */
 
 function leo(oscar){
-
+    return oscar === 88 ? "Leo finally won the oscar! Leo is happy"
+        : oscar === 86 ? "Not even for Wolf of wallstreet?!"
+        : oscar < 88 ? "When will you give Leo an Oscar?"
+        : "Leo got one already!"
 }

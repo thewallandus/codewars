7c0a2be1c37f7247bdb45ae349338fedca20693c
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-03 10:16:17 +0530

    problem 1 solution

---
+++ 3rd-October-2022/problem1.js
@@ -0,0 +1,19 @@
+/*
+
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+*/
+
+// P: parameters => an array of integers
+// R: returns => an array of integers
+// E: examples => reverseList([1,2,3,4]), [4,3,2,1]
+// P: pseudo-code => 
+
+function reverseList(list) {
+    // return list with a method that reverses the elements
+    return list.reverse()
+}

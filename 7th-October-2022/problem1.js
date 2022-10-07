a0406558e79c690930788e30a92bbe2ab690cdad
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-07 19:45:31 +0530

    problem 1 code

--- 7th-October-2022/problem1.js
+++ 7th-October-2022/problem1.js
@@ -14,3 +14,32 @@ misson 2: After misson 1 finished. you should click "Attempt" to see my three qu
 If everything is right, click "Submit" again to submit your code pass this kata.
 
 */
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return ?+?+?;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return ?+?+?;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return ?+?+?;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "";
+}
+

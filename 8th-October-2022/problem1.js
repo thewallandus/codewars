63c4509c195bc160f30b01e7bbcb8b72f845bfe1
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-08 11:54:54 +0530

    problem1 solution

--- 8th-October-2022/problem1.js
+++ 8th-October-2022/problem1.js
@@ -10,7 +10,14 @@ Examples (Input --> Output)
 "a234"   -->  false
 
 */
-
-function validatePIN (pin) {
-    
-}
+function validatePIN(str) {
+    if (str.length !== 4 && str.length !== 6) {
+      return false;
+    }
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] < '0' || str[i] > '9') {
+        return false;
+      }
+    }
+    return true;
+  }

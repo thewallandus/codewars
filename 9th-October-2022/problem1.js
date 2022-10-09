16d5a51fb7f18ccf3d17cbf5647d807864a586fe
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-09 20:07:10 +0530

    problem 1 solution

--- 9th-October-2022/problem1.js
+++ 9th-October-2022/problem1.js
@@ -17,6 +17,11 @@ Good luck!
 */
 
 function calculator(a,b,sign){
-    
-
+    if (typeof a === "number" && typeof b === "number") {
+        return sign === "+" ? a + b 
+            : sign === "*" ? a * b
+            : sign === "/" ? a / b
+            : sign === "-" ? a - b : "unknown value"
+    }
+    return "unknown value"
 }

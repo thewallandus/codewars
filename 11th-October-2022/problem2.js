1ca5abee42281ed5430b722e52e1be17d1bc1242
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-11 12:02:33 +0530

    problem 2 questioin

---
+++ 11th-October-2022/problem2.js
@@ -0,0 +1,20 @@
+/*
+
+Task
+Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+Conditions :
+N is divisible by divisor
+
+N is less than or equal to bound
+
+N is greater than 0.
+
+Notes
+The parameters (divisor, bound) passed to the function are only positive values .
+
+*/
+
+function maxMultiple(divisor, bound){
+    //your code here
+  }

44d854c9b4257c1923da8d1a296d438e8c4d968e
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-14 14:03:56 +0530

    problem 3 solution

--- 14th-October-2022/problem3.js
+++ 14th-October-2022/problem3.js
@@ -11,5 +11,10 @@ For example (Input --> Output):
 */
 
 function persistence(num) {
-    //code me
+    let i = 0;
+    while (num.toString().length !== 1) {
+      num = num.toString().split("").reduce((a,b)=>a*b);
+      i++;
+    }
+    return i;
  }

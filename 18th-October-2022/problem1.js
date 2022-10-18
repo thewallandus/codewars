02336a7623431e620f9a7339b404f7dc6df59da1
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-18 10:10:46 +0530

    problem 1 solution

--- 18th-October-2022/problem1.js
+++ 18th-October-2022/problem1.js
@@ -10,5 +10,15 @@ Example:
 */
 
 function switcheroo(x){
-
+    let str = ''
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === "a") {
+            str += 'b'
+        } else if (x[i] === "b") {
+            str += 'a'
+        } else {
+            str += 'c'
+        }
+    }
+    return str
 }

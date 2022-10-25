7b9fa3993b43ba30810dabe21a6d55d778bcfd16
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-25 10:55:55 +0530

    problem 1 first try

--- 25th-October-2022/problem1.js
+++ 25th-October-2022/problem1.js
@@ -17,5 +17,18 @@ Try to do it without using if statements!
 */
 
 function arithmetic(a, b, operator){
-    //your code here!
-  }
+    switch (operator) {
+        case "add":
+            return a + b;
+        break;
+        case "subtract":
+            return a - b;
+        break;
+        case "multiply":
+            return a * b;
+        break;
+        case "divide":
+            return a / b;
+        break;
+    }
+}

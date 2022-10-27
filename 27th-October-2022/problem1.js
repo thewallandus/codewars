a48eb3acd57e3cdf6bceb70d64a9e8fe289861d4
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-27 10:42:05 +0530

    problem 1 solution

--- 27th-October-2022/problem1.js
+++ 27th-October-2022/problem1.js
@@ -17,5 +17,17 @@ fizzbuzz(3) -->  [1, 2, "Fizz"]
 
 // Return an array
 function fizzbuzz(n) {
-  
+    const array = []
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            array.push("FizzBuzz")
+        } else if (i % 3 === 0) {
+            array.push("Fizz")
+        } else if (i % 5 === 0) {
+            array.push("Buzz")
+        } else {
+            array.push(i)
+        }
+    }
+    return array
 }

d1325b5044327a3375b46fcd70defc114a7f63f7
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-30 11:33:47 +0530

    problem 3 solution

--- 30th-October-2022/problem3.js
+++ 30th-October-2022/problem3.js
@@ -2,6 +2,10 @@
 
 A number m of the form 10x + y is divisible by 7 if and only if x − 2y is divisible by 7. In other words, subtract twice the last digit from the number formed by the remaining digits. Continue to do this until a number known to be divisible by 7 is obtained; you can stop when this number has at most 2 digits because you are supposed to know if a number of at most 2 digits is divisible by 7 or not.
 
+10x + y % 7 === 0 = x - 2y % 7 === 0
+- twice the last digit from the number formed by the remaining digits
+- 371 => 37 - (2*1)
+
 The original number is divisible by 7 if and only if the last number obtained using this procedure is divisible by 7.
 
 Examples:
@@ -23,11 +27,28 @@ Return on the stack number-of-steps, last-number-m-with-at-most-2-digits
 
 Examples:
 seven(371) should return [35, 1]
+
 seven(1603) should return [7, 2]
+160 - 6 = 154
+15 - 8 = 7
 seven(477557101) should return [28, 7]
 
 */
+function seven(m) {    
+    let count = 0;
+  
+    while(m > 99) {
+      m = parseInt(m / 10) - (2 * (m % 10)); 
+      count++;
+    }
+  
+    return [m, count];
+  }
 
-function seven(m) {
-    // your code
-}
+
+
+
+
+
+
+

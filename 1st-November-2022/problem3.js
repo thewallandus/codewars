e09da2091c42a9ae89060c627649e8f6cc74c25f
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-11-01 10:55:39 +0530

    problem 3 solution

--- 1st-November-2022/problem3.js
+++ 1st-November-2022/problem3.js
@@ -7,5 +7,7 @@ Return as a number.
 */
 
 function divCon(x){
-    
+    const numbers = x.length === 0 ? null : x.filter(item => typeof item === 'number').reduce((acc, num) => acc + num, 0)
+    const Strings =  x.length === 0 ? null : x.filter(item => typeof item === 'string').map(item => parseInt(item)).reduce((acc,num) => acc + num, 0)
+    return x.length === 0 ? 0 : Nums - Strings
 }

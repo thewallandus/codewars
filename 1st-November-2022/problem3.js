47b548871a00e54b46f1645dd873ea93089d8a73
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-11-01 10:46:10 +0530

    problem 3 question

---
+++ 1st-November-2022/problem3.js
@@ -0,0 +1,11 @@
+/*
+
+Given a mixed array of number and string representations of integers, add up the non-string integers and subtract this from the total of the string integers.
+
+Return as a number.
+
+*/
+
+function divCon(x){
+    
+}

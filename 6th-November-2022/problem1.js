77d4e6164fefb2bfac2213ace9345de94ea1ec96
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-11-06 12:34:45 +0530

    problem 1 solution

--- 6th-November-2022/problem1.js
+++ 6th-November-2022/problem1.js
@@ -18,5 +18,24 @@ Examples
 // ! [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
 function sortArray(array) {
-    //
-}
+    var odds = [];
+    //loop, if it's odd, push to odds array
+    for (var i = 0; i < array.length; ++i) {
+      if (array[i]%2 !== 0) {
+        odds.push(array[i]);
+      }
+    }
+    //sort odds from smallest to largest
+    odds.sort(function(a,b){
+      return a-b;
+    });
+    
+    //loop through array, replace any odd values with sorted odd values
+    for (var j = 0; j < array.length; ++j) {
+      if (array[j]%2 !== 0) {
+        array[j] = odds.shift();
+      }
+    }
+    
+   return array;
+  }

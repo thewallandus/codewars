65be49d592da196b3fe17196c72bd3fd546f64fd
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-11-06 11:51:34 +0530

    problem 1 prep

--- 6th-November-2022/problem1.js
+++ 6th-November-2022/problem1.js
@@ -10,6 +10,13 @@ Examples
 
 */
 
+// ! P: an array
+// ? R: an array
+// // ! EXAMPLE: 
+// ! [7, 1]  =>  [1, 7]
+// ! [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// ! [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
 function sortArray(array) {
-    // Return a sorted array.
-  }
+    //
+}

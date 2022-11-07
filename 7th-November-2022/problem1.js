b482cf206de739ef7a1afb12990658931290b073
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-11-07 12:00:09 +0530

    problem 1 answer

--- 7th-November-2022/problem1.js
+++ 7th-November-2022/problem1.js
@@ -15,15 +15,16 @@ This code is a mess! Would you help Pac to fix the code in time?
 
 */
 
-function {
-	var : Math.random()
+function yourFutureCareer () {
+	var career = Math.random()
 		if (career <= 0.32) {
-			return = FrontEnd Developer
-		 else if (career <= 0.65) 
-			return : BackEnd Developer,
+			return 'FrontEnd Developer' 
+    } else if (career <= 0.65) {
+			return 'BackEnd Developer'
 		} else {
 			return 'Full-Stack Developer'
 		}
+}
 
 
 yourFutureCareer();
